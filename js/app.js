1eb446bb52ea223ce6bac84a5036df80f60adefd
isa <isabel@juntostudio.com>
2017-05-24 16:24:53 -0400

    Make palyer and enemies appear on canvas

--- js/app.js
+++ js/app.js
@@ -1,19 +1,30 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // The image/sprite for our enemies, this uses a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+
+    //speed is pixels per second
+    var speed = 5;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // You should multiply any movement by the dt parameter which will ensure the game runs at the same speed for all computers.
+    // updates location
+    // distance is the number of pixels to move
+    var distance = this.speed * dt;
+    x = this.x + distance;
+
+    //handles collision with player
+    //if player and enemy collide {
+      //player location is initial location.
+    //}
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +35,51 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(){
+  this.sprite = 'images/char-cat-girl.png';
+  this.x = 200;
+  this.y = 400;
+  this.speed = 5;
+};
 
+Player.prototype.update = function(dt){
+  var distance = this.speed * dt;
+  x = this.x + distance;
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(i){
+  //receives user input and moves the player according to that input.
+  // left key moves player to the left, right key moves to right.. etc.
+  // handles if player tries to move offscreen
+  // handles when player reaches water by reseting back to inital location.
+  switch(i){
+  case 37:
+    x = this.x - 1;
+    break;
+  case 39:
+    x = this.x + 1;
+    break;
+  case 38:
+    y = this.y - 1;
+    break;
+  case 40:
+   y = this.y + 1;
+   break;
+  }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var enemy1 = new Enemy(10,60);
+var enemy2 = new Enemy(80,145);
+var enemy3 = new Enemy(120,230);
+var allEnemies = [enemy1, enemy2, enemy3];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
